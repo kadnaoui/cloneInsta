6579b2aae750480483953af796a1cfba9ce18bee
kadnaoui <abderrahimkdn@gmail.com>
2022-03-26 11:26:12 +0100

    adding title and pointer on projets carousel

--- src/Components/Main/Navbar.js
+++ src/Components/Main/Navbar.js
@@ -19,7 +19,7 @@ export const Navbar = (props) => {
                     <i className="fa fa-home" aria-hidden="true"></i>
                 </Link>
                 <Link to="/add" className="navigation-link" >
-                    <i className="fa fa-plus-square-o" aria-hidden="true"></i>
+                    <i className="fa fa-plus-square" aria-hidden="true"></i>
                 </Link>
                 <Link to="/suggestions" className="navigation-link" id='sug'>
                     <i className="fa fa-list" aria-hidden="true"></i>
